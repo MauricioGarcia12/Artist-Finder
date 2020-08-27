0b7b7cdd3ef03b6adcd22cef95a4c2f125242082
maumainsaw <maumainsaw@gmail.com>
2020-08-26 22:23:16 -0500

    Mostrando Cancion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 import React,{Fragment,useState,useEffect} from 'react';
 import Formulario from './components/Formulario'
-
+import Cancion from './components/Cancion'
 function App() {
   //definir state
    const[busquedaletra,guardarBusquedaLetra]=useState({});
@@ -33,6 +33,20 @@ function App() {
       <Formulario
         guardarBusquedaLetra={guardarBusquedaLetra}
       />
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-md-6'>
+
+          </div>
+          <div className='col-md-6'>
+            <Cancion
+            letra={letra}
+            />
+            
+          </div>
+        </div>
+      </div>
+
     </Fragment>
   );
 }
